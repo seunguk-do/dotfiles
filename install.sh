fde3e3851113a3e65994e1f15a078e4fa341430f
seunguk-do <seunguk.d@gmail.com>
2023-12-27 11:15:41 +0900

    Add feat: add install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-if [ ! -d $HOME/.config/nvim ]; then
-	ln -s nvim $HOME/.config/nvim
+# Installation
+## oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export ZSH_CUSTOM=${HOME}/.oh-my-zsh/custom
+
+## zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+
+## zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+
+## theme: spaceship-prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM}/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM}/themes/spaceship.zsh-theme"
+
+# Config files
+
+DIRS="nvim"
+
+## backup
+BD=${HOME}/old-dotfiles
+if [ ! -d "$BD" ]; then
+	mkdir $BD
 fi
 
-if [ ! -d $HOME/.tmux.conf ]; then
-	ln -s nvim $HOME/.tmux.conf
-fi
+mv ~/.tmux* $BD
+mv ~/.zshrc $BD
 
-if [ ! -d $HOME/.zshrc ]; then
-	# oh-my-zsh
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+for i in ${DIRS}; do
+	echo "install $i"
+	stow $i
+done
 
-	export ZSH_CUSTOM=${HOME}/.oh-my-zsh/custom
-
-	# zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-
-	# zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-
-	# theme: spaceship-prompt
-	git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${ZSH_CUSTOM}/themes/spaceship-prompt" --depth=1
-	ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM}/themes/spaceship.zsh-theme"
-
-	ln -s nvim $HOME/.zshrc
-fi
+ln -s nvim $HOME/.config/nvim
+ln -s nvim $HOME/.tmux.conf
+ln -s nvim $HOME/.zshrc
