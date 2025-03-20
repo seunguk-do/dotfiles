e68b3e25f6bf22b368d462d4a08e6611cd10e183
seunguk-do <seunguk.d@gmail.com>
2025-03-20 12:56:38 +0900

    Edit nvim config: tab

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -25,3 +25,8 @@ vim.g.clipboard = {
 }
 
 vim.g.lazyvim_python_ruff = "ruff"
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
