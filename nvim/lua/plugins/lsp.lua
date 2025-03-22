2e8886d308d10629abe5d5ee6543e6fe2038adcd
seunguk-do <seunguk.d@gmail.com>
2025-03-22 15:00:30 +0900

    Update nvim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,6 +3,22 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                diagnosticSeverityOverrides = "none",
+                typeCheckingMode = "off",
+                inlayHints = {
+                  variableTypes = false,
+                  callArgumentNames = false,
+                  functionReturnTypes = false,
+                  genericTypes = false,
+                },
+              },
+            },
+          },
+        },
         pyright = {
           mason = false,
           autostart = false,
