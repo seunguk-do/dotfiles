0c6177eb67db024a85e40a8036cb30394f374c55
seunguk-do <seunguk.d@gmail.com>
2025-03-31 13:52:47 +0900

    Fix neovim: paste related

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,39 +12,27 @@ local function paste()
   }
 end
 
-if vim.env.TMUX ~= nil then
-  vim.g.clipboard = {
-    name = "tmux",
-    copy = {
-      ["+"] = { "tmux", "load-buffer", "-w", "-" },
-      ["*"] = { "tmux", "load-buffer", "-w", "-" },
-    },
-    paste = {
-      ["+"] = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" },
-      ["*"] = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" },
-    },
-    cache_enabled = 0,
-  }
-else
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = paste,
-      ["*"] = paste,
-    },
-  }
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
+if vim.fn.has("termguicolors") == 1 then
+  -- Neovim has built-in bracketed paste mode support
+  vim.opt.paste = false
+  vim.opt.pastetoggle = ""
+
+  -- Enable termguicolors properly
+  vim.opt.termguicolors = true
 end
 
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.lazyvim_python_lsp = "basedpyright"
 
-if vim.fn.has("termguicolors") == 1 then
-  vim.opt.t_BE = vim.api.nvim_replace_termcodes([[<Esc>[?2004h]], true, true, true)
-  vim.opt.t_BD = vim.api.nvim_replace_termcodes([[<Esc>[?2004l]], true, true, true)
-  vim.opt.t_PS = vim.api.nvim_replace_termcodes([[<Esc>[200~]], true, true, true)
-  vim.opt.t_PE = vim.api.nvim_replace_termcodes([[<Esc>[201~]], true, true, true)
-end
